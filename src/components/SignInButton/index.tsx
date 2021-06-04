285e9d30beb3c259810ccbb8f66bd16872907a4f
rebecapedrosa <rebequinhanico@outlook.com.br>
2021-06-03 21:41:57 -0400

    fix: finish sign in button styles

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,12 +1,20 @@
-import {FaGithub} from 'react-icons/fa'
-import styles from './styles.module.scss'
+import { FaGithub } from "react-icons/fa";
+import {FiX} from 'react-icons/fi'
+
+import styles from "./styles.module.scss";
 export function SignInButton() {
-    return (
-        <button 
-        className={styles.signInButton}
-        type="button">
-            <FaGithub/>
-            Sign in with Github
-        </button>
-    )
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="84d361" />
+      Nome do user
+      <FiX color="#737380" className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="eba417" />
+      Sign in with Github
+    </button>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <>
+    </>
   );
 }
