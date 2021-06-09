9c5cd70386cfa76d397fb8c5e0fedfd53a7f2c76
rebecapedrosa <rebequinhanico@outlook.com.br>
2021-06-08 20:50:29 -0400

    feat: get static props

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,11 +1,13 @@
-import styles from './style.module.scss'
-export function SubscribeButton(){
-    return(
-        <button
-            type="button"
-            className={styles.subscribeButton}
-        >
-            Subscribe now
-        </button>
-    )
+import styles from "./style.module.scss";
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import Head from "next/head";
+import { GetStaticProps } from "next";
 import { SubscribeButton } from "../components/SubscribeButton";
 
-import styles from './home.module.scss'
+import styles from "./home.module.scss";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -12,15 +21,38 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
           <p>
-            Get access to all the publications<br/>
-            <span>for $9.90 month</span>
+            Get access to all the publications
+            <br />
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
-        <img src="/images/avatar.svg" alt="Girl coding"/>
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1IztMTA9ht0orOeGgDVKgn6c");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount/100)
+  };
+
+  return {
+    props: {
+      product,
+    },
+    //quanto tempo deve demorar a reconstrução da pagina
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
