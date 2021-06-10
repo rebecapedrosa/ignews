f733264abe0237357728afaf74da95a33aa4757e
rebecapedrosa <rebequinhanico@outlook.com.br>
2021-06-10 15:13:41 -0400

    feat: connect app with faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,31 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth";
+import { signIn } from "next-auth/client";
+import Providers from "next-auth/providers";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      scope: "read:user",
     }),
   ],
-})
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), {
+            data: { email },
+          })
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
