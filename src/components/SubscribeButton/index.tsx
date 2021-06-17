b40c85395c329f7ff9b7ee94aee8f443175f9b11
rebecapedrosa <rebequinhanico@outlook.com.br>
2021-06-16 21:33:49 -0400

    feat: stripe checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -18,12 +18,14 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
     try {
       const response = await api.post("/subscribe");
-
+      
       const { sessionId } = response.data;
+      
 
       const stripe = await getStripeJs();
 
-      await stripe.redirectToCheckout(sessionId);
+      await stripe.redirectToCheckout({sessionId});
+
     } catch (err) {
       alert(err.message);
     }
